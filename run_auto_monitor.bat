5911ac48e857c5e168245428b5143731a9d61a06
WuXiangM <1615372377@qq.com>
2025-07-29 09:16:32 +0800

    2025-07-29 09:16 自动提交电费变更

--- run_auto_monitor.bat
+++ run_auto_monitor.bat
@@ -19,5 +19,15 @@ echo 【正在运行主脚本...】
 python src\nju_electric_monitor_auto.py
 
 echo.
+echo 【检测文件变更并自动提交到GitHub...】
+git status
+for /f "tokens=1-2 delims=." %%a in ('wmic os get localdatetime ^| findstr /r /c:"^[0-9]"') do set dt=%%a
+set commitmsg=%dt:~0,4%-%dt:~4,2%-%dt:~6,2% %dt:~8,2%:%dt:~10,2% 自动提交电费变更
+
+git add .
+git commit -m "%commitmsg%"
+git push
+
+echo.
 echo 【按任意键退出...】
 pause >nul
